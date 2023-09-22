665d90a72edc8efe872f160caa368d393897ea88
anton <a.solovjev@vzv.su>
2023-09-22 16:43:24 +0300

    Изменына модель для tg users и добавлены запросы подписки

--- telegram_users/models.py
+++ telegram_users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class TelegramUser(models.Model):
-    name = models.CharField(max_length=250, blank=True, null=True)
-    telegram_id = models.BigIntegerField(unique=True)
-    created_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=250, blank=True, verbose_name='Имя пользователя')
+    telegram_id = models.BigIntegerField(unique=True, verbose_name='ID пользователя в телеграмме')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    request_number = models.IntegerField(default=0, verbose_name='Количество запросов')
+    subscription = models.BooleanField(default=False, verbose_name='Подписка')
 
     def __str__(self):
-        return self.telegram_id
+        return str(self.telegram_id)
