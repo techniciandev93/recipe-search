98fff74b6b5d1f541d03563b697d9e920bc1022f
anton <anton@adminpc.Dlink>
2023-09-23 13:02:17 +0300

    дбавленого обновление только 1 поля request_number

--- telegram_users/services.py
+++ telegram_users/services.py
@@ -5,7 +5,6 @@ def create_user(user_id, user_name):
     TelegramUser.objects.get_or_create(name=user_name, telegram_id=user_id)
 
 
-
 def subscribe(user_id):
     tg_user = TelegramUser.objects.get(telegram_id=user_id)
     if tg_user.subscription:
@@ -28,10 +27,11 @@ def get_request_number(user_id):
     user = TelegramUser.objects.get(telegram_id=user_id)
     return user.request_number
 
+
 def add_requests_number(user_id):
     user = TelegramUser.objects.get(telegram_id=user_id)
     user.request_number += 1
-    user.save()
+    user.save(update_fields=["request_number"])
 
 
 def is_subscribe(user_id):
